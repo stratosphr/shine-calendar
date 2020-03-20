19cc359343115b865673afb9ee3f73069a49c3ce
stratosphr <stratosphr@gmail.com>
2020-03-20 23:38:57 +0100

    Displayed calendar with custom start and end dates

--- src/components/SCalendar.vue
+++ src/components/SCalendar.vue
@@ -1,17 +1,36 @@
 <template>
-  <p>Hello from calendar</p>
+  <v-sheet height="300">
+    <v-calendar
+        :end="end"
+        :short-intervals="false"
+        :start="start"
+        :weekdays="[1, 2, 3, 4, 5, 6]"
+        locale="fr"
+        ref="calendar"
+        type="week"
+    >
+    </v-calendar>
+  </v-sheet>
 </template>
 
 <script>
+	import moment from 'moment'
+
 	export default {
 		name: 'SCalendar',
 
 		created() {
 			console.clear()
+			moment.locale('fr')
 		},
 
-		mounted() {
-			console.log('Mounteds')
+		computed: {
+			start() {
+				return moment('2020-03-20').startOf('week').format('YYYY-MM-DD')
+			},
+			end() {
+				return moment('2020-03-23').endOf('week').format('YYYY-MM-DD')
+			}
 		}
 
 	}
