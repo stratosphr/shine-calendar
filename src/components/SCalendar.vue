4afa812ef584df85470b0d60cf4c181af9fe90fa
stratosphr <stratosphr@gmail.com>
2020-03-21 13:47:01 +0100

    Added and styled events containers

--- src/components/SCalendar.vue
+++ src/components/SCalendar.vue
@@ -1,28 +1,13 @@
 <template>
-  <v-container class="pt-12 px-3">
-    <v-sheet height="300">
-      <v-row no-gutters>
-        <v-col>
-          <v-slider
-              :max="60"
-              :min="1"
-              hide-details
-              label="Minutes par interval"
-              thumb-label="always"
-              v-model="intervalMinutes"
-          />
-          <v-slider
-              :max="60"
-              :min="1"
-              hide-details
-              label="Hauteur des intervals"
-              thumb-label="always"
-              v-model="intervalHeight"
-          />
-        </v-col>
-      </v-row>
+  <div>
+    <v-sheet
+        height="300"
+        style="user-select: none"
+    >
       <v-calendar
           :end="end.format('YYYY-MM-DD')"
+          :event-ripple="false"
+          :events="eventsContainers"
           :first-interval="firstInterval"
           :interval-count="intervalCount"
           :interval-height="intervalHeight"
@@ -34,26 +19,63 @@
           ref="calendar"
           type="week"
       >
+        <template #event="{event, day}">
+          <div class="text-wrap">
+            <div>
+              {{`Start: ${event.start}`}}
+              {{`End: ${event.end}`}}
+            </div>
+            <div>
+              {{`Date: ${day.date}`}}
+            </div>
+          </div>
+        </template>
       </v-calendar>
     </v-sheet>
-  </v-container>
+    <v-row class="mt-10">
+      <v-col>
+        <v-slider
+            :max="60"
+            :min="1"
+            hide-details
+            label="Minutes par interval"
+            thumb-label="always"
+            v-model="intervalMinutes"
+        />
+      </v-col>
+      <v-col>
+        <v-slider
+            :max="60"
+            :min="1"
+            hide-details
+            label="Hauteur des intervals"
+            thumb-label="always"
+            v-model="intervalHeight"
+        />
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
-	import moment from 'moment'
+	import Moment           from 'moment'
+	import { extendMoment } from 'moment-range'
+
+	const moment = extendMoment(Moment)
+	moment.locale('fr')
 
 	export default {
 		name: 'SCalendar',
 
 		data: () => ({
 			firstInterval: 0,
-			intervalMinutes: 15,
+			intervalMinutes: 60,
 			intervalCount: 24,
 			intervalHeight: 30
 		}),
 
 		created() {
-			moment.locale('fr')
+			console.clear()
 		},
 
 		computed: {
@@ -62,12 +84,29 @@
 			},
 			end() {
 				return moment().endOf('week')
+			},
+			eventsContainers() {
+				return [...moment.range(this.start, this.end).by('day')].map(day => ({
+					start: day.format('YYYY-MM-DD 00:00'),
+					end: day.format('YYYY-MM-DD 24:00')
+				}))
 			}
 		}
 
 	}
 </script>
 
-<style scoped>
+<style>
+  /*noinspection CssUnusedSymbol*/
+  .v-event-timed-container {
+    margin: 0 !important;
+  }
 
+  /*noinspection CssUnusedSymbol*/
+  .theme--light .v-calendar-events .v-event-timed, .theme--dark .v-calendar-events .v-event-timed {
+    padding: 0 !important;
+    border: 0 solid !important;
+    cursor: default !important;
+    border-radius: 0 !important;
+  }
 </style>
---
+++ src/components/SCalendarEvent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Event
+  </div>
+</template>
+
+<script>
+	export default {
+		name: 'SCalendarEvent'
+	}
+</script>
+
+<style scoped>
+
+</style>
