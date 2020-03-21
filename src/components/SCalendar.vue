15499b88e044d144db0573082dd44b1a4b78f66e
stratosphr <stratosphr@gmail.com>
2020-03-21 12:06:19 +0100

    Added sliders to control intervals minutes and height

--- src/components/SCalendar.vue
+++ src/components/SCalendar.vue
@@ -1,20 +1,42 @@
 <template>
-  <v-sheet height="300">
-    <v-calendar
-        :end="end.format('YYYY-MM-DD')"
-        :first-interval="firstInterval"
-        :interval-count="intervalCount"
-        :interval-height="intervalHeight"
-        :interval-minutes="intervalMinutes"
-        :short-intervals="false"
-        :start="start.format('YYYY-MM-DD')"
-        :weekdays="[1, 2, 3, 4, 5, 6]"
-        locale="fr"
-        ref="calendar"
-        type="week"
-    >
-    </v-calendar>
-  </v-sheet>
+  <v-container class="pt-12 px-3">
+    <v-sheet height="300">
+      <v-row no-gutters>
+        <v-col>
+          <v-slider
+              :max="60"
+              :min="1"
+              hide-details
+              label="Minutes par interval"
+              thumb-label="always"
+              v-model="intervalMinutes"
+          />
+          <v-slider
+              :max="60"
+              :min="1"
+              hide-details
+              label="Hauteur des intervals"
+              thumb-label="always"
+              v-model="intervalHeight"
+          />
+        </v-col>
+      </v-row>
+      <v-calendar
+          :end="end.format('YYYY-MM-DD')"
+          :first-interval="firstInterval"
+          :interval-count="intervalCount"
+          :interval-height="intervalHeight"
+          :interval-minutes="intervalMinutes"
+          :short-intervals="false"
+          :start="start.format('YYYY-MM-DD')"
+          :weekdays="[1, 2, 3, 4, 5, 6]"
+          locale="fr"
+          ref="calendar"
+          type="week"
+      >
+      </v-calendar>
+    </v-sheet>
+  </v-container>
 </template>
 
 <script>
@@ -24,9 +46,9 @@
 		name: 'SCalendar',
 
 		data: () => ({
+			firstInterval: 0,
 			intervalMinutes: 15,
-			intervalCount: 3,
-			firstInterval: 22,
+			intervalCount: 24,
 			intervalHeight: 30
 		}),
 
