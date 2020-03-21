fc622d693799f882f29805260dbb0190fed72b22
stratosphr <stratosphr@gmail.com>
2020-03-21 17:33:59 +0100

    Displayed custom scrollbars on events when necessary

--- src/components/SCalendar.vue
+++ src/components/SCalendar.vue
@@ -21,19 +21,19 @@
       >
         <template #event="{day}">
           <div
-              class="text-wrap"
+              class="text-wrap s-calendar"
               style="position: relative"
               v-if="$refs.calendar"
           >
             <!-- EVENTS -->
             <div
                 :style="{
-            	position: 'absolute',
-            	left: 0,
-            	right: 0,
-            	...geometryForDate(event, moment(day.date))
+                  position: 'absolute',
+                  left: 0,
+                  right: 0,
+                  ...geometryForDate(event, moment(day.date))
                 }"
-                class="orange pa-2 overflow-hidden"
+                class="orange pa-1 overflow-auto s-calendar-event"
                 v-for="event in optimizedEvents[day.date]"
             >
               <slot
@@ -149,5 +149,31 @@
     border: 0 solid !important;
     cursor: default !important;
     border-radius: 0 !important;
+  }
+
+  /* Track */
+  .s-calendar-event::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    background-color: #F5F5F5;
+    border-radius: 7px;
+  }
+
+  /* Scrollbar */
+  .s-calendar-event::-webkit-scrollbar {
+    width: 7px;
+    background-color: transparent;
+  }
+
+  /* Handle */
+  .s-calendar-event::-webkit-scrollbar-thumb {
+    border-radius: 7px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, .3);
+    background-image: -webkit-gradient(linear, left bottom, left top, color-stop(0, rgb(123, 175, 239)), color-stop(1, rgb(78, 108, 198)));
+  }
+
+  /* Handle on hover */
+  .s-calendar-event::-webkit-scrollbar-thumb:hover {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, .5);
+    background-image: -webkit-gradient(linear, left bottom, left top, color-stop(0, rgb(123, 175, 239)), color-stop(1, rgb(78, 108, 198)));
   }
 </style>
